76e62cdcca7261ad6571b3beca74e6eda50b0f0f
ATHL337 <81762936+ATHL337@users.noreply.github.com>
2023-01-17 15:06:38 -0500

    user input sanitization
    Updated to include strncpy function to copy the user input into the input buffer, but only copies the specified number of characters, which is the size of the buffer minus one to leave space for the null-terminating character

--- mamacita.c
+++ mamacita.c
@@ -4,35 +4,34 @@
 
 void process_download() {
  FILE *fp = fopen("abuelita.gif", "wb");
-    if (fp) {
-        fwrite(Dancing_Granny_gif, 1, Dancing_Granny_gif_len, fp);
-        fclose(fp);
-        printf("File abuelita.gif created successfully\n");
-        printf("Make sure you spread the word! Visit https://bio.link/raicescy\n");
-    } else {
-        printf("Error creating file abuelita.gif\n");
-    }
+ if (fp) {
+  fwrite(Dancing_Granny_gif, 1, Dancing_Granny_gif_len, fp);
+  fclose(fp);
+  printf("File abuelita.gif created successfully\n");
+  printf("Make sure you spread the word! Visit https://bio.link/raicescy\n");
+ } 
+ else {
+  printf("Error creating file abuelita.gif\n");
+ }
 }
 
 int main() {
-    char input[8];
-    printf("Enter text: ");
-    fgets(input, sizeof(input), stdin);
-    
-    
-    int len = strlen(input);
-    if( input[len-1] == '\n')
-        input[len-1] = 0;
-
-    char bandera[] = {0x66, 0x6c, 0x61, 0x67, 0x7b, 0x66, 0x66, 0x34, 0x31, 0x61, 0x31, 0x64, 0x30, 0x30, 0x33, 0x30, 0x39, 0x31, 0x65, 0x31, 0x39, 0x39, 0x63, 0x33, 0x30, 0x37, 0x64, 0x39, 0x38, 0x63, 0x62, 0x31, 0x65, 0x38, 0x35, 0x64, 0x38, 0x7d};
-    if (strcmp(input, "ATHL337") == 0) {
-        printf("%s\n",bandera);
-     } else if(strcmp(input,"HYPE")==0){
-        process_download();
-    } 
-    else {
-        printf("TRY HARDER\n");
+ char input[8];
+ printf("Enter text: ");
+ if (fgets(input, sizeof(input), stdin) != NULL) {
+  strncpy(input, input, sizeof(input) - 1);
+  // remove the newline character
+  input[strlen(input) - 1] = 0;
+  
+  char bandera[] = {0x66, 0x6c, 0x61, 0x67, 0x7b, 0x66, 0x66, 0x34, 0x31, 0x61, 0x31, 0x64, 0x30, 0x30, 0x33, 0x30, 0x39, 0x31, 0x65, 0x31, 0x39, 0x39, 0x63, 0x33, 0x30, 0x37, 0x64, 0x39, 0x38, 0x63, 0x62, 0x31, 0x65, 0x38, 0x35, 0x64, 0x38, 0x7d};
+  if (strcmp(input, "ATHL337") == 0) {
+   printf("%s\n",bandera);
+  } else if(strcmp(input,"HYPE")==0){
+   process_download();
+  }
+  else {
+   printf("TRY HARDER\n");
+  }
+  return 0;
     }
-
-    return 0;
-}
+ }
